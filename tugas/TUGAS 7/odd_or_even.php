52aaac44525674cdd25b2e86cac9e157cef069c4
AyangNovaAnggraeni <165558797+AyangNovaAnggraeni@users.noreply.github.com>
2025-05-22 22:55:36 +0700

    Update odd_or_even.php

--- tugas/TUGAS 7/odd_or_even.php
+++ tugas/TUGAS 7/odd_or_even.php
@@ -1,8 +1,17 @@
 <?php
-$number = 5;
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    $number = (int) $_POST["number"];
+    echo "Number : $number<br>";
 
-$status = ($number % 2 == 0) ? "number : even" : "number : odd";
+    $status = ($number % 2 == 0) ? "Type : even" : "Type : odd";
 
-echo $number. "<br>"; 
-echo $status;
+    echo $status;
+}
 ?>
+
+<form method="post">
+    <label for="number">Enter a number :</label>
+    <input type="number" name="number" id="number" required>
+    <input type="submit" value="Odd / Even Checker">
+</form>
