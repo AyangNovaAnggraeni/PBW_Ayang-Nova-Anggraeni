381cfc1798d95c2a40b5631512c5ffd5b9ba3a74
AyangNovaAnggraeni <165558797+AyangNovaAnggraeni@users.noreply.github.com>
2025-05-22 19:11:45 +0700

    Create header.php

---
+++ tugas/TUGAS 7/header.php
@@ -0,0 +1,11 @@
+<html>
+   <body>
+       <div class="menu">
+           <h2>Include<h2>
+           <?php include 'menu.php'; ?>
+           <br>
+           <h2>Require<h2>
+           <?php require 'menu.php'; ?>
+       </div>
+   </body>
+</html>
