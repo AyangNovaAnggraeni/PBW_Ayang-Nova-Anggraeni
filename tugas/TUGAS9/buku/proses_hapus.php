663be1107dd545f680bf7d21cbdbaf62c841bc4a
unknown <you@example.com>
2025-06-13 22:19:35 +0700

    Menambahkan folder TUGAS9 ke dalam folder tugas

---
+++ tugas/TUGAS9/buku/proses_hapus.php
@@ -0,0 +1,32 @@
+
+<?php
+include ('../koneksi_db.php');
+
+if (isset($_GET['id'])) {
+    $id = intval($_GET['id']);
+
+    // Get the book data before deleting
+    $result = $conn->query("SELECT * FROM buku WHERE ID = $id");
+    if ($result->num_rows > 0) {
+        $book = $result->fetch_assoc();
+
+        // Insert the book data into backup table
+        $stmt = $conn->prepare("INSERT INTO hapus_buku (ID, Judul, Penulis, Tahun_Terbit, Harga) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("issid", $book['ID'], $book['Judul'], $book['Penulis'], $book['Tahun_Terbit'], $book['Harga']);
+        $stmt->execute();
+        $stmt->close();
+
+        // Delete the book from original table
+        $conn->query("DELETE FROM buku WHERE ID = $id");
+
+        header("Location: index.php?message=deleted");
+        exit();
+    } else {
+        echo "Data not found.";
+    }
+} else {
+    echo "Invalid request.";
+}
+?>
+
+
---
+++ tugas/TUGAS9/nav.php
@@ -0,0 +1,64 @@
+<!-- Menu Navigasi -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+       <div class="container">
+           <a class="navbar-brand" href="/TUGAS9/index.php">Toko Buku Online</a>
+           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+           </button>
+           <div class="collapse navbar-collapse" id="navbarNav">
+               <ul class="navbar-nav ms-auto">
+                   <li class="nav-item">
+                       <a class="nav-link" href="/TUGAS9/index.php">Daftar Buku</a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="/TUGAS9/buku/tambah_buku.php">Tambah Buku</a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="/TUGAS9/pelanggan/pelanggan.php">Pelanggan</a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="/TUGAS9/transaksi/transaksi.php">Buat Pesanan</a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="/TUGAS9/transaksi/lihat_transaksi.php">Lihat Pesanan</a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="/TUGAS9/buku/hapus.php">Hapus Buku</a>
+                   </li>
+               </ul>
+           </div>
+       </div>
+</nav>
+
+
+<!-- Menu Navigasi -->
+<!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+       <div class="container">
+           <a class="navbar-brand" href="../index.php">Toko Buku Online</a>
+           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+           </button>
+           <div class="collapse navbar-collapse" id="navbarNav">
+               <ul class="navbar-nav ms-auto">
+                   <li class="nav-item">
+                       <a class="nav-link" href="../index.php">Daftar Buku</a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="../buku/tambah_buku.php">Tambah Buku</a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="../pelanggan/pelanggan.php">Pelanggan</a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="../transaksi/transaksi.php">Buat Pesanan</a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="../transaksi/lihat_transaksi.php">Lihat Pesanan</a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="../buku/hapus.php">Hapus Buku</a>
+                   </li>
+               </ul>
+           </div>
+       </div>
+</nav> -->
